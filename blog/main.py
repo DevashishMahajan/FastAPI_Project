417f3f3084e750f280697a54b9e44046893b8798
DevashishMahajan <devashishmahajan31@gmail.com>
2022-10-25 19:11:32 +0530

    main.py added with ORM of sqlalchemy

--- blog/main.py
+++ blog/main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI,Depends
-from . import schemas, models
-from . database import engine, sessionLocal
+#pip install -r requirements.txt
+
+from fastapi import FastAPI,Depends,status,Response,HTTPException
+import schemas, models
+from database import engine, SessionLocal
 from sqlalchemy.orm import Session
 
 #make instance of FastAPI 
@@ -8,6 +10,7 @@ app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
+# Connect to database
 def get_db():
     db = SessionLocal()
     try:
@@ -15,8 +18,9 @@ def get_db():
     finally:
         db.close()
 
+# Post a blog and add it into database
 @app.post('/blog')
-def create(request: schemas.blog, db: Session = Depends(get_db)):
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
     db.commit()
@@ -24,7 +28,7 @@ def create(request: schemas.blog, db: Session = Depends(get_db)):
 
     return new_blog
 
-
+"""
 @app.get('/blog/{id}', status_code=200, response_model=schemas.ShowBlog)
 def show (id, response : Response, db: Session= Depends(get_db)):
     blog= db.query(models.Blog).filter(models.Blog.id= id).first()
@@ -34,12 +38,21 @@ def show (id, response : Response, db: Session= Depends(get_db)):
                             detail= f"Blog with the id {id} is not available")
             
     return blog
+"""
+
+# Get all blogs details
+@app.get('/blog')
+def all(db: Session = Depends(get_db)):
+    blogs= db.query(models.Blog).all()
+    return blogs
 
 
 # Add user and user detailes to database
 #use password hashing
-from passlib.CryptContext(schemas=["bcrypt"], deprecated="auto")
+#from passlib. import CryptContext
+from passlib.context import CryptContext
 
+pwd_cxt = CryptContext(schemes=["bcrypt"], deprecated="auto")
 @app.post('/user')
 def create_user(request: schemas.User, db: Session=Depends(get_db)):
     hashedPassword=pwd_cxt.hash(request.password)
